78acce90d4cd1d92c1330408781e9c63a45c8186
Richard <richard232manansala@gmail.com>
2023-09-03 15:30:43 +0800

    Fixed middleware bug

--- middleware/authenticate.ts
+++ middleware/authenticate.ts
@@ -1,6 +1,7 @@
 export default defineNuxtRouteMiddleware((to, from) => {
+  if (process.client) return;
   const user = useSupabaseUser();
-  if (!user.value && to.path !== "/landing") {
+  if (user.value == undefined && to.path !== "/landing") {
     return navigateTo("/landing");
   }
 });
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
 <script setup lang="ts">
 
     definePageMeta({
-        middleware: ["auth"]
+        middleware: ["authenticate"]
     })
 
     const user = useSupabaseUser()
