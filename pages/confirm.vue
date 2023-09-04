9a7b270844acb42943e8d70ad7cd5e3aa48895b6
Richard <richard232manansala@gmail.com>
2023-09-04 10:02:26 +0800

    Added back the watch, because useSupabaseUser requires it

--- pages/confirm.vue
+++ pages/confirm.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
-
-  await useFetch("/api/user/create/post")
-  navigateTo('/')
+  const {data: {value}, error} = await useFetch("/api/user/create/post")
+  if(error.value) {
+    console.log("ERROR creating user at dtabase, retrying")
+    await useFetch("/api/user/create/post")
+  }
+  const user = useSupabaseUser()
+  watch([user], () => {
+    if (user.value) {
+      navigateTo('/')
+    }
+  }, {immediate: true,})
 
 </script>
 <template>
