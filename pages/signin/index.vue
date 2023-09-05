c5acf9aa3950f06ade94cb0e5362f2939d738279
Richard <richard232manansala@gmail.com>
2023-09-05 20:05:44 +0800

    Changed the signin to redirect to /signin/confirm

--- pages/signin/index.vue
+++ pages/signin/index.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
+import { SupportedProviders } from '@sidebase/nuxt-auth/dist/runtime/composables/authjs/useAuth';
+
     const {signIn, data} = useAuth()
     watch(data, () => {
         console.log(data.value)
     })
+    function handleSignin(prov : SupportedProviders) {
+        signIn(prov, {callbackUrl: "/signin/confirm"})
+    }
 </script>
 
 <template>
     <div>
         Sign in now
-        <button @click="() => signIn('github')">Sign in with Github</button>
+        <button @click="handleSignin('google')">Sign in with Google</button>
     </div>
 </template>
