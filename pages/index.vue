8effbc0c5d5014f9ab8ee8d91d0261b52ee757b7
Richard <richard232manansala@gmail.com>
2023-09-05 20:07:10 +0800

    Added a simple auth middleware in index

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
 
 <script setup lang="ts">
+    definePageMeta({
+        middleware: [
+            function(to, from) {
+                const {data} = useAuth()
+                if(data.value?.user == null) {
+                    return navigateTo('/signin')
+                }
+            }
+        ]
+    })
     const {data} = useAuth()
     console.log(data.value?.user)
 </script>
