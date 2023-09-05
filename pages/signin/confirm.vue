6ca9e1aab02bdd3b51d780a5429a60508a66233f
Richard <richard232manansala@gmail.com>
2023-09-05 20:00:59 +0800

    Added confirmation route

---
+++ pages/signin/confirm.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+    const {data} = useAuth()
+    watch(data, () => {
+        if(data.value?.user !== undefined) {
+            return navigateTo('/')
+        }
+    }, {immediate : true})
+</script>
+
+
+<template>
+    <div>
+        Logging in!
+    </div>
+</template>
