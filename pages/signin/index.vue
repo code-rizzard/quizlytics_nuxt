5f52e28c290fc9e3ce2dcafe3bee8fedec2d2b0f
Richard <richard232manansala@gmail.com>
2023-09-07 21:26:01 +0800

    Added no-auth middleware

--- pages/signin/index.vue
+++ pages/signin/index.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import { SupportedProviders } from '@sidebase/nuxt-auth/dist/runtime/composables/authjs/useAuth';
-
+    definePageMeta({
+        middleware: [
+            "no-auth"
+        ]
+    })
     const {signIn, data} = useAuth()
     watch(data, () => {
         console.log(data.value)
